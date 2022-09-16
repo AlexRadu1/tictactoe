07e0d7133fac12e22954e263980af9961e2c2db2
alex <alexeuno1@yahoo.com>
2022-09-16 12:07:02 +0300

    Modified board from 0 8 to 1 9

--- main.rb
+++ main.rb
@@ -17,7 +17,7 @@ end
 class Game
   WINNING = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
   def initialize()
-    @board=[0,1,2,3,4,5,6,7,8]
+    @board=[1,2,3,4,5,6,7,8,9]
     puts "What is first players's name?"
     @player1=Player.new(gets.chomp,"X")
     @player1.show_player_info()
@@ -30,40 +30,51 @@ class Game
     puts @board.each_slice(3).map { |row| row.join(" | ") }.join("\n" + "--+---+--" + "\n")
   end
   def play()
-    turnCount=2
-    0.upto(@board.length) do
-      if turnCount % 2 == 0
-        play_turn(@player1)
-        puts "#{@player1.points}"
-          if game_won?(@player1) 
-            print_board()
-            puts "#{@player1.name} won! GG"
-            break
-          end  
-        else
-        play_turn(@player2)
-        puts "#{@player2.points}"
-          if game_won?(@player2) 
-            print_board()
-            puts "#{@player2.name} won! GG"
-            break
-          end  
+    play=true
+    loop do
+      turnCount=2
+      0.upto(@board.length-1) do
+        if turnCount % 2 == 0
+          play_turn(@player1)
+            if game_won?(@player1) 
+              print_board()
+              puts "#{@player1.name}(#{@player1.marker}) won! GG"
+              break
+            end  
+          else
+          play_turn(@player2)
+            if game_won?(@player2) 
+              print_board()
+              puts "#{@player2.name}(#{@player2.marker}) won! GG"
+              break
+            end  
+        end
+        print_board()
+        turnCount+=1
+        puts "It`s a draw " if turnCount==11;
       end
-      print_board()
-      turnCount+=1
+      puts "Play again? (y/n)"
+      answer=gets.chomp
+      play=false if answer=="n";
+      break if play==false ;
+      if play==true
+        @player1.points=Array.new()
+        @player2.points=Array.new()
+        @board=[1,2,3,4,5,6,7,8,9]
+        print_board()
+      end  
     end
-    
   end  
-# 0 2 3 6 
+
   def game_won?(player)
     WINNING.any? do |line|
       line.all? { |pos| player.points[pos] == player.marker }
     end
   end
-
   def play_turn(player)
-    puts "#{player.name} select your position:"
-        selection = gets.to_i
+    
+    puts "#{player.name}(#{player.marker}) select your position:"
+        selection = gets.to_i - 1
         if is_ocupied?(selection) || selection > 8
           puts "Invalid position.Try again! "
           play_turn(player)
